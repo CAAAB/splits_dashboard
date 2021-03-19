995e87cf17c028b24c7cb4508890a3ec633f3ecf
CAAAB <charles.borel@alumni.epfl.ch>
2021-03-19 23:03:51 +0100

    Update splits_dashboard.py

--- splits_dashboard.py
+++ splits_dashboard.py
@@ -16,36 +16,9 @@ alpha = .05
 def print_prediction(res):
     return f'{100*(1-alpha):.0f}% chance of ending {res["endsplit_name"]} between {nice_time(res["hpd_low"])} and {nice_time(res["hpd_high"])}'
 
-def plot_future_splits(runner, split=None, current_time=None):
-        res = []
-        for endsplit in np.arange(0, runner.split_map['split_id'].iloc[-1]+1):
-            res.append(runner.predict(0, 0, endsplit)) # Issue here, current_time should be time at end of split 0, not 0
-        res = pd.DataFrame(res)
-        res['display_name'] = [f'{row.endsplit_id} - {row.endsplit_name}' for _,row in res.iterrows()]
-        res['text'] = [f'{row.display_name}<br>High: {nice_time(row.hpd_high)}<br>Median: {nice_time(row.hpd_median)}<br>Low: {nice_time(row.hpd_low)}' for _,row in res.iterrows()]
-
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=res['display_name'], y=res['hpd_low']-res['hpd_median'], line_color='Black', text=res['text'], hoverinfo='text', mode='lines'))
-        fig.add_trace(go.Scatter(x=res['display_name'], y=res['hpd_high']-res['hpd_median'], line_color='Black', text=res['text'], hoverinfo='text', fill='tonexty', mode='lines'))
-        #fig.add_trace(go.Scatter(x=res['display_name'], y=[0]*res.shape[0], line_color='Black', mode="lines"))
-
-        if split is not None and current_time is not None:
-            res = []
-            for endsplit in np.arange(split, runner.split_map['split_id'].iloc[-1]+1):
-                res.append(runner.predict(split, current_time, endsplit))
-            res = pd.DataFrame(res)
-            res['display_name'] = [f'{row.endsplit_id} - {row.endsplit_name}' for _,row in res.iterrows()]
-            res['text'] = [f'{row.display_name}<br>High: {nice_time(row.hpd_high)}<br>Median: {nice_time(row.hpd_median)}<br>Low: {nice_time(row.hpd_low)}' for _,row in res.iterrows()]
-
-            fig.add_trace(go.Scatter(x=res['display_name'], y=res['hpd_low']-res['hpd_median'], line_color='Black', text=res['text'], hoverinfo='text', mode='lines'))
-            fig.add_trace(go.Scatter(x=res['display_name'], y=res['hpd_high']-res['hpd_median'], line_color='Black', text=res['text'], hoverinfo='text', fill='tonexty', mode='lines'))
-            #fig.add_trace(go.Scatter(x=res['display_name'], y=[0]*res.shape[0], line_color='Black', mode="lines"))
-        fig.update_layout(showlegend=False, template="plotly_white", yaxis_title="Likely seconds range")
-        return fig
-
 def main():
             
-    @st.cache(suppress_st_warning=True)
+    @st.cache(suppress_st_warning=True, allow_output_mutation=True)
     def get_runner(runner_name):
         return Runner(runner_name, alpha=alpha, q=.95)
 
