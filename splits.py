0137972541e00c8b62a86e4cf2c02400428b8df6
CAAAB <charles.borel@alumni.epfl.ch>
2021-03-19 22:53:00 +0100

    Update splits.py

--- splits.py
+++ splits.py
@@ -408,7 +408,7 @@ class Runner:
         fig.show()
         return fig
     
-    def plot_future_splits(self, split=None, current_time=None):
+    def plot_future_splits(self, split="", current_time=""):
         res = []
         for endsplit in np.arange(0, self.split_map['split_id'].iloc[-1]+1):
             res.append(self.predict(0, 0, endsplit)) # Issue here, current_time should be time at end of split 0, not 0
@@ -417,12 +417,11 @@ class Runner:
         res['text'] = [f'{row.display_name}<br>High: {nice_time(row.hpd_high)}<br>Median: {nice_time(row.hpd_median)}<br>Low: {nice_time(row.hpd_low)}' for _,row in res.iterrows()]
 
         fig = go.Figure()
-        fig.add_trace(go.Scatter(x=res['display_name'], y=res['hpd_low']-res['hpd_median'], line_color='Black', text=res['text'], hoverinfo='text', mode='lines'))
-        fig.add_trace(go.Scatter(x=res['display_name'], y=res['hpd_high']-res['hpd_median'], line_color='Black', text=res['text'], hoverinfo='text', fill='tonexty', mode='lines'))
+        fig.add_trace(go.Scatter(x=res['display_name'], y=res['hpd_low']-res['hpd_median'], line_color='Blue', text=res['text'], hoverinfo='text', mode='lines'))
+        fig.add_trace(go.Scatter(x=res['display_name'], y=res['hpd_high']-res['hpd_median'], line_color='Blue', text=res['text'], hoverinfo='text', fill='tonexty', mode='lines'))
         #fig.add_trace(go.Scatter(x=res['display_name'], y=[0]*res.shape[0], line_color='Black', mode="lines"))
-        fig.update_layout(showlegend=False, template="plotly_white")
 
-        if split is not None and current_time is not None:
+        if split != "" and current_time != "":
             res = []
             for endsplit in np.arange(split, self.split_map['split_id'].iloc[-1]+1):
                 res.append(self.predict(split, current_time, endsplit))
@@ -430,9 +429,9 @@ class Runner:
             res['display_name'] = [f'{row.endsplit_id} - {row.endsplit_name}' for _,row in res.iterrows()]
             res['text'] = [f'{row.display_name}<br>High: {nice_time(row.hpd_high)}<br>Median: {nice_time(row.hpd_median)}<br>Low: {nice_time(row.hpd_low)}' for _,row in res.iterrows()]
 
-            fig.add_trace(go.Scatter(x=res['display_name'], y=res['hpd_low']-res['hpd_median'], line_color='Black', text=res['text'], hoverinfo='text', mode='lines'))
-            fig.add_trace(go.Scatter(x=res['display_name'], y=res['hpd_high']-res['hpd_median'], line_color='Black', fill='tonexty', mode='lines'))
+            fig.add_trace(go.Scatter(x=res['display_name'], y=res['hpd_low']-res['hpd_median'], line_color='Gold', text=res['text'], hoverinfo='text', mode='lines'))
+            fig.add_trace(go.Scatter(x=res['display_name'], y=res['hpd_high']-res['hpd_median'], line_color='Gold', text=res['text'], hoverinfo='text', fill='tonexty', mode='lines'))
             #fig.add_trace(go.Scatter(x=res['display_name'], y=[0]*res.shape[0], line_color='Black', mode="lines"))
-        fig.update_layout(showlegend=False, template="plotly_white", yaxis_title="Possible seconds saved")
+        fig.update_layout(showlegend=False, template="plotly_white", yaxis_title="Likely seconds range")
         return fig
 
