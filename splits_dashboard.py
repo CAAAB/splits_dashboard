89c77902af7185a5d14123155bbd9b03578d0b5e
CAAAB <charles.borel@alumni.epfl.ch>
2021-03-19 18:13:46 +0100

    Update splits_dashboard.py

--- splits_dashboard.py
+++ splits_dashboard.py
@@ -12,6 +12,9 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
+def print_prediction(res):
+    return f'{1-res.alpha}% chance of ending {res.endsplit_name} between {nice_time(res.hpd_low)} and {nice_time(res.hpd_high)}'
+
 def main():
             
     @st.cache(suppress_st_warning=True)
@@ -24,22 +27,17 @@ def main():
     split_list = list(runner.split_map.split_code)
     fig_violin = runner.boxplot(points = "outliers")
     st.write(fig_violin)
-    st.write(runner.plot_splits_over_time('W', q=.05)) # Split improvement over time
-    st.write(runner.plot_resets()) # Number of resets    split_list = list(runner.split_map.split_code.values)
     
-    chosen_split = st.selectbox('Choose split:', split_list)#, default = [split_list[0]])
+    chosen_split = st.selectbox('Choose split:', split_list, index=0)
     chosen_split_id = runner.split_map.loc[runner.split_map.split_code == chosen_split,"split_id"].values[0]
-    st.write(chosen_split_id)
     current_time = st.text_input(f"{chosen_split} end time:")
-    chosen_endsplit = st.selectbox('Choose target split:', split_list)#, default = [split_list[0]])
+    chosen_endsplit = st.selectbox('Choose target split:', split_list, index=len(split_list))#, default = [split_list[0]])
     chosen_endsplit_id = runner.split_map.loc[runner.split_map.split_code == chosen_endsplit,"split_id"].values[0]
-    st.write(chosen_endsplit_id)
     res=runner.predict(chosen_split_id,process_time(current_time), chosen_endsplit_id, display = False, verbose=False)
-    st.write(res)
-    st.write(runner.plot_splits_over_time('M', q=.05))
-    st.write(runner.plot_resets())
-
-    st.write(runner.split_analysis('average_run'))
+    st.write(print_prediction(res))
+    st.write(runner.plot_splits_over_time('M', q=.05)) # Split improvement over time
+    st.write(runner.plot_resets()) # Number of resets
+    st.write(runner.split_analysis('average_run')) # Average run
     #split= "Palace Done"
     #current_time = process_time("2:43:47")
     #res = runner.predict(split, current_time, endsplit=None,display=False, verbose=True)
