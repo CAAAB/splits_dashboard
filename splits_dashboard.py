a65a0024e5201c01d7ef56c3a61f26defdf56fa0
CAAAB <charles.borel@alumni.epfl.ch>
2021-03-19 22:58:15 +0100

    Update splits_dashboard.py

--- splits_dashboard.py
+++ splits_dashboard.py
@@ -63,9 +63,13 @@ def main():
     chosen_split_id = runner.split_map.loc[runner.split_map.split_code == chosen_split_code,"split_id"].values[0]
     chosen_split_name = runner.split_map.loc[runner.split_map.split_code == chosen_split_code,"split_name"].values[0]
 
-    # Target split
+    # Last split time
     current_time = st.text_input(f"{chosen_split_name} end time", "00:00:00")
-    pct = process_time(current_time) if current_time != "" else ""
+    if current_time != "":
+        st.write(current_time)
+        pct = process_time(current_time)
+    
+    # Target split
     chosen_endsplit_code = st.selectbox('Target split', split_list, index=len(split_list)-1)
     chosen_endsplit_id = runner.split_map.loc[runner.split_map.split_code == chosen_endsplit_code,"split_id"].values[0]
     chosen_endsplit_name = runner.split_map.loc[runner.split_map.split_code == chosen_endsplit_code,"split_name"].values[0]
